edf9a63735fa13b326d339133ad1583250f4ed6f
augustindmbl <augustin.demontbel@gmail.com>
2024-12-26 09:42:04 +0100

    Mise en forme du code

---
+++ Calculation.py
@@ -0,0 +1,80 @@
+import datetime as dtm
+
+def TDMAsolver(a, b, c, d):
+
+    """Solve a tridiagonal system of equations AX = D using the Thomas algorithm.
+
+    Parameters:
+    a : list of floats
+        Sub-diagonal elements of the matrix (a[0] is unused -> Must add a 0 at the beginning of the list).
+    b : list of floats
+        Diagonal elements of the matrix.
+    c : list of floats
+        Super-diagonal elements of the matrix (c[-1] is not used in calculations, but the list should have the same length as b and d).
+    d : list of floats
+        Right-hand side of the equation.
+
+    Returns:
+    x : list of floats
+        Solution to the system.
+    """
+    
+    n = len(d)
+
+    # Make copies of the input arrays to avoid modifying them
+    cp = c[:]
+    dp = d[:]
+
+    # Forward elimination
+    for i in range(1, n):
+        w = a[i] / b[i - 1]
+        b[i] -= w * cp[i - 1]
+        dp[i] -= w * dp[i - 1]
+
+    # Back substitution
+    x = [0] * n
+    x[-1] = dp[-1] / b[-1]
+    for i in range(n - 2, -1, -1):
+        x[i] = (dp[i] - cp[i] * x[i + 1]) / b[i]
+
+    return x
+
+def runtime_program (beginning_date_and_time):
+
+    """Calculates and prints program runtime based on start time.
+
+    Parameters
+    ----------
+    beginning_date_and_time : datetime.datetime
+    
+    Returns
+    -------
+    None
+    """
+
+    ending_date_and_time=dtm.datetime.now()
+    runtime = ending_date_and_time - beginning_date_and_time
+
+    #type(runtime) =  datetime.timedelta, it should be convert in a string to be easily manipulated and printed
+    string_runtime = str(runtime)
+
+    hour = string_runtime.split(":")[0]
+    minute = string_runtime.split(":")[1]
+    second = str(round(float(string_runtime.split(":")[2]),2))
+
+    # These different cases are made to avoid printing 00h00min00,4s for the shortest program (just for the visual aspect)
+    if hour == "0":
+
+        if minute == "00":
+
+            print("Runtime : "+second+"s")
+
+        else:
+
+            print("Runtime : "+minute+"min"+second+"s")
+    
+    else:
+
+        print("Runtime : "+hour+"h"+minute+"min"+second+"s")
+    
+    return None
--- main.py
+++ main.py
@@ -1,60 +1,92 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import datetime as dtm
+import AnalyticalSolution
+import NumericalSolution
+import Calculation
+import Graph
 
 
 ### Initialization ###
 
-## Discretization parameters ##
+beginning_date_and_time = dtm.datetime.now()
 
-Lx, Ly = 1, 1
+## Discretization ##
+
+Lx, Ly = 10*10**(-2), 10*10**(-2)
 Nx, Ny = 100, 100
 dx, dy, = Lx/Nx, Ly/Ny
-dt = 0
+
+simulation_time = 0.5
+dt = 0.01
+
 
 ## Physical parameters ##
 
 temperature_init = 50
 temperature_T1 = 150
 temperature_T2 = 100
-thermal_diffusivity = 0
-temperature_list = np.zeros((Nx, Ny))
+temperature_init_list = np.zeros((Nx, Ny))*temperature_init
+
+# For a steel plate
+thermal_conductivity = 30 # (in W.m^-1.K^-1)
+density = 800  # (in kg.m^-3)
+heat_capacity = 520  # (in J.kg^-1.K^-1)
+
+thermal_diffusivity = thermal_conductivity / (density * heat_capacity)  # (in m^2.s^-1)
+
 
 ## Meshgrid ##
 
 x_list, y_list = np.linspace(0, Lx, Nx), np.linspace(0, Ly, Ny)
 
 
-## Definition of the infinite sum ##
+## Boundary conditions ##
 
-def term_the_sum (n, x, y):
-
-    n_term = (1/(2*n + 1))*np.sin((2*n + 1)*np.pi*x/Lx)*np.sinh((Ly - y)*(2*n + 1)*np.pi/Lx)/np.sinh((2*n + 1)*np.pi*Ly/Lx)
-
-    return n_term
-
-def infinite_sum (x, y):
-
-    index = 0
-    sum = 0
-
-    while abs(term_the_sum(index, x, y) - term_the_sum(index + 1, x, y)) > 10**(-10):
-
-        sum += term_the_sum(index, x, y)
-        index += 1
-    
-    return sum
-
-### Calculation of the analytical solution ###
-
-for i, x in enumerate(x_list):
-
-    for j, y in enumerate(y_list):
-
-        temperature = temperature_T2 + 4*(temperature_T1 - temperature_T2)*infinite_sum(x, y)/np.pi 
-
-        temperature_list[i, j] = temperature
+temperature_init_list[:, 0] = temperature_T1
+temperature_init_list[:, -1] = temperature_T2
+temperature_init_list[0, :] = temperature_T2
+temperature_init_list[-1, :] = temperature_T2
 
 
-plt.matshow(temperature_list, cmap = "hot")
-plt.colorbar()
+##Simulation parameters ##
+
+x_fourrier_coeff = thermal_diffusivity*dt/(dx**2)
+y_fourrier_coeff = thermal_diffusivity*dt/(dy**2)
+
+### Solutions ###
+
+## Analytical solutions ##
+
+temperature_analytical_list = AnalyticalSolution.solution(x_list, y_list, Lx, Ly, temperature_T1, temperature_T2, temperature_init_list.copy())
+
+
+## Numerical solutions ##
+
+x_a_lign, x_b_lign, x_c_lign, y_a_lign, y_b_lign, y_c_lign = NumericalSolution.system_matrix(Nx, Ny, x_fourrier_coeff, y_fourrier_coeff)
+temperature_numerical_list = NumericalSolution.solution(simulation_time, Nx, Ny, dt, x_fourrier_coeff, y_fourrier_coeff, x_list, y_list, temperature_init_list.copy(), 
+                                                        temperature_T1, temperature_T2, x_a_lign, x_b_lign, x_c_lign, y_a_lign, y_b_lign, y_c_lign)
+
+
+## Difference between analytical and numerical solution ##
+
+temperature_error = np.abs(temperature_analytical_list - temperature_numerical_list)
+
+
+### Plotting ###
+figure = plt.figure("result_heat_2D")
+
+Graph.heatmap_plot(figure, temperature_analytical_list, "Solution analytique", 1)
+Graph.heatmap_plot(figure, temperature_numerical_list, "Solution numérique", 2)
+Graph.heatmap_plot(figure, temperature_error, "|Analytique - Numérique|", 3)
+
+plt.suptitle("Conduction instationnaire en 2D dans une plaque d'acier (simulation de " + str(simulation_time)+"s)", fontsize = 16)
+
+plt.subplots_adjust(left=0.05, right=0.95, top=0.55, bottom=0.15)
+
+### Runtime ###
+Calculation.runtime_program(beginning_date_and_time)
+
+
+
 plt.show()
